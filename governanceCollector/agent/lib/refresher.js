05725c9a3a3cf441278172d8a671551d3a71937e
Travis Gibson <travis@aculytics.net>
2018-09-07 15:35:39 -0400

    Change refresher.js to export object instead of single function

--- governanceCollector/agent/lib/refresher.js
+++ governanceCollector/agent/lib/refresher.js
@@ -10,7 +10,6 @@ const loggerObject = {
     jsFile: "refresher.js"
 }
 
-
 function logMessage(level, msg) {
     if (level == "info" || level == "error") {
         socketHelper.sendMessage("governanceCollector", msg);
@@ -20,19 +19,21 @@ function logMessage(level, msg) {
 
 let start_time, end_time;
 
-function reloadApp(config, taskname) {
-    return new Promise(function (resolve, reject) {
-        return qrsCalls.qrsReloadTask(config, taskname)
-            .then(function (result) {
-                logMessage("info", result);
-                resolve(result);
-            })
-            .catch(function (error) {
-                logMessage("error", "Error executing " + taskname);
-                logMessage("error", JSON.stringify(error));
-                reject(error);
-            })
-    });
+let refresher = {
+    reloadApp: function (config, taskname) {
+        return new Promise(function (resolve, reject) {
+            return qrsCalls.qrsReloadTask(config, taskname)
+                .then(function (result) {
+                    logMessage("info", result);
+                    resolve(result);
+                })
+                .catch(function (error) {
+                    logMessage("error", "Error executing " + taskname);
+                    logMessage("error", JSON.stringify(error));
+                    reject(error);
+                })
+        });
+    }
 }
 
-module.exports = reloadApp;
+module.exports = refresher;
