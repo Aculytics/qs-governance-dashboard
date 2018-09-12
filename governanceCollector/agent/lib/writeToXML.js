a00c7c889f4df171b634a7271b1ee5766deed88c
Travis Gibson <travis@aculytics.net>
2018-09-12 15:50:35 -0400

    Add XML attribute name check on XML parse error

--- governanceCollector/agent/lib/writeToXML.js
+++ governanceCollector/agent/lib/writeToXML.js
@@ -18,12 +18,74 @@ function writeToXML(root, type, data, appId, options, morePath) {
 
     morePath = morePath || "";
 
-    var xmlData = js2xmlparser.parse(root, data, options);
+    try {
+        var xmlData = js2xmlparser.parse(root, data, options);
+    }
+    catch (err) {
+        data = replaceInvalidChars(data);
+        var xmlData = js2xmlparser.parse(root, data, options);
+
+        //logger.error(err, loggerObject);
+        //fs.writeFileSync(path.join(config.agent.metadataPath, morePath, (appId !== undefined ? appId + "_" : "") + type + ".err.json"), JSON.stringify(data));
+    }
+    
 
 
     fs.writeFileSync(path.join(config.agent.metadataPath, morePath, (appId !== undefined ? appId + "_" : "") + type + ".xml"), xmlData);
     logger.debug("Wrote file: " + path.join(config.agent.metadataPath, morePath, (appId !== undefined ? appId + "_" : "") + type + ".xml"), loggerObject);
     return appId + "_" + type + ".xml file saved";
+}
+
+function replaceInvalidChars(data) {
+    var newData = JSON.stringify(data);
+
+    var propNames = getPropNames(data);
+    var propStringCheck = /^[A-Za-z._:][A-Za-z0-9._:]*$/i;
+    var propFirstCharCheck = /^[A-Za-z._:]/i;
+    var propOtherCharsCheck = /^[A-Za-z0-9._:]*$/i;
+
+    propNames.forEach(function(prop) {
+        if (!propStringCheck.test(prop)) {
+            var newPropName = prop;
+
+            if (!propOtherCharsCheck.test(prop)) {
+                var stringSplit = newPropName.split('');
+
+                stringSplit.forEach(function (char) {
+                    if (!propOtherCharsCheck.test(char)) {
+                        newPropName = newPropName.replace(char, '_');
+                    }
+                });
+            }
+
+            if (!propFirstCharCheck.test(newPropName)) {
+                newPropName = '_' + newPropName;
+            }
+
+            newData = newData.replace(prop, newPropName);
+            logger.warn(`Invalid XML attribute name detected, replacing ${prop} with ${newPropName}`);
+        }
+    });
+
+    //fs.writeFileSync(path.join(config.agent.metadataPath, morePath, (appId !== undefined ? appId + "_" : "") + type + ".err.json"), JSON.stringify(propNames));
+    return JSON.parse(newData);
+
+    function getPropNames(obj) {
+        var result = [];
+        for (var prop in obj) {
+            var value = obj[prop];
+
+            if (typeof value === 'object') {
+                result = result.concat(getPropNames(value));
+            }
+            else if(!Array.isArray(obj)) {
+                result = result.concat(Object.keys(obj));
+            }
+        }
+
+        // Removes all duplicate prop names before returning
+        return Array.from(new Set(result));
+    }
 }
 
 module.exports = writeToXML;
